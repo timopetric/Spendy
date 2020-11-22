61de212b40e1721484cbe71ee8d0c081dd72a5b4
Kristjan <ks4681@student.uni-lj.si>
2020-11-22 12:56:48 +0100

    napaka pri uvozu js za stran add_expenses

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -12,13 +12,13 @@ const add_expenses = (req, res) => {
   res.render('add_expenses',{
     title: 'Dodaj',
     stylesheets_load: ["https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css"],
-    scripts_load: []
+    scripts_load: ["https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"]
   });
 };
 const analysis = (req, res) => {
   res.render('analysis',{
     title: 'Analiza',
-    stylesheets_load: ["https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css"],
+    stylesheets_load: [""],
     scripts_load: ["https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js",
       "/javascripts/first-page-chart.js", "/javascripts/analysis_script.js"]
   });
