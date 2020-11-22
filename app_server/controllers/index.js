b2d6352419f62f4d11b8417fca5b2b8bd6d5d7be
Kristjan <ks4681@student.uni-lj.si>
2020-11-22 12:28:09 +0100

    dodane add_expenses, analysis ter graphs handlebars predloge in ustrezne javascript datoteke

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -8,6 +8,33 @@ const index = (req, res) => {
   });
 };
 
+const add_expenses = (req, res) => {
+  res.render('add_expenses',{
+    title: 'Dodaj',
+    stylesheets_load: ["https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css"],
+    scripts_load: []
+  });
+};
+const analysis = (req, res) => {
+  res.render('analysis',{
+    title: 'Analiza',
+    stylesheets_load: ["https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css"],
+    scripts_load: ["https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js",
+      "/javascripts/first-page-chart.js", "/javascripts/analysis_script.js"]
+  });
+};
+const graphs = (req, res) => {
+  res.render('graphs',{
+    title: 'Grafično',
+    stylesheets_load: ["https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css"],
+    scripts_load: ["https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js",
+      "/javascripts/first-page-chart.js", "/javascripts/graphs_script.js"]
+  });
+};
+
 module.exports = {
-  index
+  index,
+  add_expenses,
+  analysis,
+  graphs,
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -9,5 +9,8 @@ const ctrlIndex = require('../controllers/index'); // first page controller
 // GET first page
 router.get('/first_page', ctrlFirstPage.page);
 router.get('/index', ctrlIndex.index);
+router.get('/add_expenses', ctrlIndex.add_expenses);
+router.get('/analysis', ctrlIndex.analysis);
+router.get('/graphs', ctrlIndex.graphs);
 
 module.exports = router;
