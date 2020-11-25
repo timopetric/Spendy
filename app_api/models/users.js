35aaf4d8712e48218945cf88283669bd973f14d5
timopetric <timopetric@hotmail.com>
2020-11-25 11:49:02 +0100

    db shcema for real

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -1,18 +1,74 @@
 const mongoose = require('mongoose');
 
+var Schema = mongoose.Schema;
 
-const userSettings = new mongoose.Schema({
-  language: String, // sl, en
-  currency: String  // eur, usd
-});
+// skupine:
+// -ime
+// -admini
+// -stroski
+//
+// strosek:
+// -boolean: prihodek/odhodek
+// -skupina (kam pase) oziroma uporabnik
+//
+// uporabnik:
+// -ime
+// -priimk
+// -mail
+// -pass
+// -skupine
+// -(stroški)?
+
 
 const userSchema = new mongoose.Schema({
   username: {type: String, required: true},
-  name: String,
-  surname: String,
-  balance: Number,
-  settings: userSettings
+  name: {type: String, required: true},
+  surname: {type: String, required: true},
+  mail: {type: String, required: true},
+  pass: {type: String, required: true},
+  balance: {type: Number, required: true},
+  groupIds: [{type: Schema.Types.ObjectId, ref: "Group"}] // user belongs to multiple groups
+},
+{
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  },
+  collection: "Users"
 });
+mongoose.model("User", userSchema);
 
 
-mongoose.model('User', userSchema, 'Users');
+const expenseSchema = new mongoose.Schema({
+  isExpenditure: {type: Boolean, required: true},
+  cost: {type: Number, required: true},
+  date: {type: Date, required: true},
+  category_name: {type: String, required: true},
+  groupId: {type: Schema.Types.ObjectId, ref: "Group", required: true}, // expense belongs to a certain group
+  description: String,
+},
+{
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  },
+  collection: "Expenses"
+});
+mongoose.model("Expense", expenseSchema);
+
+
+const groupSchema = new mongoose.Schema({
+  balance: {type: Number, "default": 0.0, required: true},
+  name: {type: String, required: true},
+  userIds: [{type: Schema.Types.ObjectId, ref: "User"}],  // users of the group
+  adminIds: [{type: Schema.Types.ObjectId, ref: "User"}], // admins (users) of the group
+},
+{
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  },
+  collection: "Groups"
+});
+mongoose.model("Group", groupSchema);
+
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,6 +7,7 @@ const ctrlUser = require('../controllers/users');
 // rest api for user (implement CRUD)
 router.get('/v1/users', ctrlUser.getAllUsers);
 router.post('/v1/users', ctrlUser.addUser);
+router.post('/v1/groups', ctrlUser.addGroup);
 // router.get('/v1/users/:userId', ctrlUser.getUserById);
 // router.put('/v1/users/:userId', ctrlUser.updateUser);
 // router.delete('/v1/users/:userId', ctrlUser.deleteUser);
