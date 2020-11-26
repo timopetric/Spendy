e3bf54582d868bc21f830a2def51828d2f49c7e6
janeznovak <ag5319@student.uni-lj.si>
2020-11-26 19:49:58 +0100

    Dodan getUserById

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -9,8 +9,8 @@ const ctrlGroups = require("../controllers/groups");
 // rest api for user (implement CRUD)
 router.get('/v1/users', ctrlUser.getAllUsers);
 router.post('/v1/users', ctrlUser.addUser);
-router.post('/v1/groups', ctrlUser.addGroup);
-router.get('/v1/users/:userId', ctrlUser.getUserById);
+//router.post('/v1/groups', ctrlUser.addGroup);
+router.get('/v1/users/:id', ctrlUser.getUserById);
 // router.put('/v1/users/:userId', ctrlUser.updateUser);
 // router.delete('/v1/users/:userId', ctrlUser.deleteUser);
 
