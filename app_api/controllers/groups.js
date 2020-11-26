81cd1dd061b32e2dd73c0cc0b714156e26fc358d
Matic Bregar <matic.bregar98@gmail.com>
2020-11-26 21:35:36 +0100

    cleaning files

--- app_api/controllers/groups.js
+++ app_api/controllers/groups.js
@@ -3,7 +3,6 @@ const User = mongoose.model("User");
 const Expense = mongoose.model("Expense");
 const Group = mongoose.model("Group");
 
-
 const getAllGroups = async (req, res) => {
   let found = await Group.find({});
   console.log(found);
@@ -18,7 +17,7 @@ const getGroupById = async (req, res) => {
     .exec((napaka, group) => {
       if (!group) {
         return res.status(404).json({
-          "message": "Ne najdem skupine s podanim id-jem",
+          message: "Ne najdem skupine s podanim id-jem",
         });
       } else if (napaka) {
         return res.status(500).json(napaka);
@@ -76,57 +75,36 @@ const addGroup = async (req, res) => {
     });
 };
 
-const addGroupExpenseById = (req, res) => {
-  const groupId = req.params.groupId;
-  const expenseId = req.params.expenseId;
-
-  Expense.findById(expenseId).exec((napaka, expense) => {
-    if (!lokacija) {
-      return res.status(404).json({
-        "message":
-          "Ne najdem expensa s podanim enoličnim identifikatorjem idLokacije.",
-      });
-    } else if (napaka) {
-      return res.status(500).json(napaka);
-    }
-
-    res.status(200).json(expense);
-  });
-};
-
 const removeGroupById = (req, res) => {
-  const {idGroup} = req.params;
+  const { idGroup } = req.params;
   // const idGroup = req.params.idGroup;
   console.log(idGroup);
   if (idGroup) {
-    Group
-        .findByIdAndRemove(idGroup)
-        .exec((err, group) => {
-          if (err) {
-            return res.status(500).json(err);
-          }
-          console.log("GROUP DELETED: "+group);
-          console.log("Expenses to delete: "+group.expenses);
+    Group.findByIdAndRemove(idGroup).exec((err, group) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      console.log("GROUP DELETED: " + group);
+      console.log("Expenses to delete: " + group.expenses);
 
-          Expense.deleteMany({_id: { $in: (group.expenses)}}, (err, res) => {
-            if (!res || !err) {
-              console.log("Error deleting expenses for group."+err);
-            }
-          });
-          
-          res.status(204).json(null);
-        });
+      Expense.deleteMany({ _id: { $in: group.expenses } }, (err, res) => {
+        if (!res || !err) {
+          console.log("Error deleting expenses for group." + err);
+        }
+      });
+
+      res.status(204).json(null);
+    });
   } else {
     res.status(404).json({
-      "message": "groupId is a required parameter."
-    })
+      message: "groupId is a required parameter.",
+    });
   }
-}
-
+};
 
 module.exports = {
   getAllGroups,
   getGroupById,
   addGroup,
-  removeGroupById
+  removeGroupById,
 };
--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -54,42 +54,6 @@ const addUser = (req, res) => {
   );
 };
 
-// const addGroup = (req, res) => {
-//   const reqName = req.body.name; // name of the group
-//   const balance = 0.0; // balance of the group
-//   const reqUsers = req.body.userIds; // list of users (user ids)
-//   const reqAdmins = req.body.adminIds; // list of admins (user ids)
-//
-//   Group.create(
-//     {
-//       balance: balance,
-//       name: reqName,
-//     },
-//     (error, group) => {
-//       if (error) {
-//         res.status(400).json(error);
-//       } else {
-//         // add users from reqUsers to the group
-//         for (const uId of reqUsers) {
-//           if (validateUserById(uId)) {
-//             group.userIds.push(uId);
-//             group.adminIds.push(uId);
-//           } else {
-//             return res.status(500).json("Uporabnik: " + uId + " ne obstaja.");
-//           }
-//         }
-//
-//         group.save(function (err) {
-//           // res.status(400).json(err);
-//           console.log("Error saving to database! " + err);
-//         });
-//
-//         return res.status(201).json(group);
-//       }
-//     }
-//   );
-// };
-
 function validateUserById(userId) {
   let user = User.findById(userId).exec((err, user) => {
     if (err) {
