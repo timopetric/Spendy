9239a927b3f88ddc2b8f01adcc5660d4728d46f8
janeznovak <ag5319@student.uni-lj.si>
2020-11-28 00:46:06 +0100

    Popravljen checkSignUp

--- public/javascripts/checkLogin.js
+++ public/javascripts/checkLogin.js
@@ -4,6 +4,12 @@ function prevent() {
     });
 }
 
+function validateEmail(email) {
+    //var re = /^[a-zA-Z0-9!@#$%\^&*)(+=._-]*$/;
+    var re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return re.test(email);
+}
+
 function validate() {
     var email = $("#email").val();
     var passwd = $("#password").val();
@@ -12,13 +18,20 @@ function validate() {
     if (email === "") {
         odgovor += "\nVnesite epoštni naslov!";
     }
+
+    if (!validateEmail(email)) {
+        odgovor += "\nEmail ni prave oblike";
+        //prevent();
+    }
+
     if (passwd === "") {
         odgovor += "\nVnesite geslo!";
         console.log("geslo je prazno");
-        prevent();
+        //prevent();
     }
 
     if (odgovor !== "") {
+        prevent();
         alert(odgovor);
     }
 }
