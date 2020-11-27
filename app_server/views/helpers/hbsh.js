e588e0a4b3afc71aea9ffb0e52dc5affb6b8c80a
Kristjan <ks4681@student.uni-lj.si>
2020-11-27 19:16:36 +0100

    dodana helper hbs funkcija IF

--- app_server/views/helpers/hbsh.js
+++ app_server/views/helpers/hbsh.js
@@ -1,1 +1,9 @@
 const hbs = require('hbs');
+
+hbs.registerHelper("if", function(conditional, options) {
+    if (conditional) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
