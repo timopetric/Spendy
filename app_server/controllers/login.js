5958f223134919c7a58837b7fce075a0f38aec21
timopetric <timopetric@hotmail.com>
2020-11-27 20:55:49 +0100

    add sidebar button highlighting to login/signup pages

--- app_server/controllers/login.js
+++ app_server/controllers/login.js
@@ -2,6 +2,7 @@
 const login = (req, res) => {
   res.render('login',{
     title: 'Prijavna stran',
+    navbar_button_selected_login: true,
     stylesheets_load: ["/stylesheets/style.css", "/stylesheets/style-profil.css"],
     scripts_load: ["/javascripts/checkLogin.js"]
   });
--- app_server/controllers/signup.js
+++ app_server/controllers/signup.js
@@ -1,6 +1,7 @@
 const signup = (req, res) => {
   res.render('signup',{
     title: 'Registracijska stran',
+    navbar_button_selected_signup: true,
     stylesheets_load: ["/stylesheets/style.css", "/stylesheets/style-profil.css"],
     scripts_load: []
   });
