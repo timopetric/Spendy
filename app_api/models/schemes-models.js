0cf01defc4dec64d281543ae0db7ccb8888b349e
timopetric <timopetric@hotmail.com>
2020-11-29 15:11:42 +0100

    add unique to user mail

--- app_api/models/schemes-models.js
+++ app_api/models/schemes-models.js
@@ -73,7 +73,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     surname: { type: String, required: true },
-    mail: { type: String, required: true},
+    mail: { type: String, required: true, unique: true }, // todo: delete mongo atlas SpendyDB to add unique constraint
     pass: { type: String, required: true /*, set: Data.prototype.saltySha1*/ },
     balance: { type: Number, required: true },
     groupIds: [{
