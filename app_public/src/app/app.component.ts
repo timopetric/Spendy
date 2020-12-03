4f4b22b013319bad6c6a4cf5048bf1bc966802c2
timopetric <timopetric@hotmail.com>
2020-12-03 20:28:13 +0100

    add html break lines angular pipe

--- app_public/src/app/app.component.ts
+++ app_public/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
+    selector: 'app-root', // to je selector <> tag v index.html
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HtmlBreakLinesPipe } from './html-break-lines.pipe';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HtmlBreakLinesPipe],
     imports: [BrowserModule],
     providers: [],
-    bootstrap: [AppComponent],
+    bootstrap: [AppComponent], // to komponento zaganjamo (mora biti izvožena (export class))
 })
 export class AppModule {}
+
+// Trenutno smo aplikacijo pognali s pomočjo ukaza
+// ng serve
+// , ki poskrbi, da se proces generiranja Angular aplikacije
+// v jezik JavaScript izvede v pomnilniku, zato JavaScript datotek ne bomo našli nikjer v mapah projekta.
+// Če bi želeli generirati Angular aplikacijo v jeziku JavaScript bi uporabili ukaz
+// ng build
+// Za potrebe razvoja je uporaba ng serve idealna, saj se spremembe spletne strani samodejno
+// naložijo ob spremembi izvorne kode.
