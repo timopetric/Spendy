0d04a85d114fd298b2a51df6a1718ffa9c3a8077
timopetric <timopetric@hotmail.com>
2020-12-29 23:35:08 +0100

    Revert "angular build"
    This reverts commit 74ecd9d5906d608c3fab3cc9420d4d79d3987445.

--- app_public/build/assets/javascripts/searchInput.js
+++
@@ -1,9 +0,0 @@
-function onClick() {
-    let input = document.getElementById("searchLogin");
-    console.log(input.value);
-    if (window.location.href.includes("?")) {
-        window.location = window.location.href + "&search=" + `${input.value}`;
-    } else {
-        window.location = window.location.href + "?search=" + `${input.value}`;
-    }
-}
