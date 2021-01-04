c501129d8a415e8abebe41b726376b95359c5237
timopetric <timopetric@hotmail.com>
2021-01-04 04:21:43 +0100

    ng build heroku

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use(passport.initialize());
 
 // CORS
 app.use("/api", (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://sp-spendy.herokuapp.com"); // http://localhost:3000/ https://sp-spendy.herokuapp.com,
+    res.header("Access-Control-Allow-Origin", "*"); // http://localhost:3000/ https://sp-spendy.herokuapp.com, https://sp-spendy.herokuapp.com
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT,OPTIONS");
     next();
